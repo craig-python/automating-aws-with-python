8b861f2f3df2b567643eec316e9deb35e26ee169
Craig Dalrymple <craig.dalrymple@btinternet.com>
2020-07-06 16:11:44 +0100

    Add notifier serverless project

--- 02-notifon/ec2_example.py
+++ 02-notifon/ec2_example.py
@@ -2,29 +2,36 @@
 import boto3
 session = boto3.Session(profile_name='pythonAutomation')
 ec2 = session.resource('ec2')
+
+# Create a key pair
 key_name = 'python_automation_key'
 key_path = key_name + '.pem'
 key = ec2.create_key_pair(KeyName=key_name)
 key.key_material
 with open(key_path, 'w') as key_file:
     key_file.write(key.key_material)
-    
+
 get_ipython().run_line_magic('ls', '-l python_automation_key.pem')
+
 import os, stat
 os.chmod(key_path, stat.S_IRUSR | stat.S_IWUSR)
 get_ipython().run_line_magic('ls', '-l python_automation_key.pem')
+
+# Find an AMI
 list(ec2.images.filter(Owners=['amazon']))
 len(list(ec2.images.filter(Owners=['amazon'])))
 ami_name = 'amzn-ami-hvm-2018.03.0.20180508-x86_64-gp2'
 img = ec2.Image('ami-09d95fab7fff3776c')
+img = ec2.Image('ami-0e9089763828757e1')
 img.name
 ami_name = 'amzn2-ami-hvm-2.0.20200520.1-x86_64-gp2'
+ami_name = 'amzn-ami-hvm-2018.03.0.20200602.1-x86_64-gp2'
 ami_name
-filters = [{'Name:': 'name', 'Values': [ami_name]}]
-list(ec2.images.filter(Owners=['amazon'], Filters=filters))
 filters = [{'Name': 'name', 'Values': [ami_name]}]
 list(ec2.images.filter(Owners=['amazon'], Filters=filters))
 img
+
+# Create an Instance
 instances = ec2.create_instances(ImageId=img.id, MinCount=1, MaxCount=1, InstanceType='t2.micro', KeyName = key.key_name)
 instances
 ec2.Instance(id='i-01ed269c364f7c7c6')
